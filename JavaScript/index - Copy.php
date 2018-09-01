b34c14a47d346fe591b6bc9f89c57ef957938ed1
Ribela <39341446+Ribela2018@users.noreply.github.com>
2018-09-01 12:54:22 -0400

    Add files via upload

---
+++ JavaScript/index - Copy.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- estilos css boostrap etc -->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<title> PRACTICA JAVA SCRIPT </title>
+</head>
+<body>
+<div class="container text-center py-5">
+	<h1 class="display-3 py-5">Practica de Java Script</h1>
+	<button class="btn btn-danger" id="boton" onclick="alert('Esto es una alerta creada con Java Script')"> "Haz click y veras lo que hace Java Script"</button>
+	<div class="my-5" id="alerta">	</div>
+
+</div>
+
+<script>
+	
+alert("Esto es una alerta creada con Java Script")
+
+</script>
+
+
+<!-- Jquery y JS -->
+<script type="text/javascript" src="js/jquery-3.3.1.slim.min.js"></script>
+<script type="text/javascript" src="js/tether.min.js" ></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<!-- <script type="text/javascript" src="js/scriptpractico.js"></script> -->
+
+</body>
+</html>
