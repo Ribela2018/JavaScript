1a4ce6540808eb8589a3933cec2a29aa4916fd7e
Ribela <39341446+Ribela2018@users.noreply.github.com>
2018-09-01 12:51:03 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- estilos css boostrap etc -->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/stylo.css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<title> PRACTICA JAVA SCRIPT </title>
+</head>
+<body>
+	<br><br>
+
+	<!-- practica de array con js  -->
+ <!--<div class="container text-center py-5" name="Boton1"> 
+	<h1 class="display-3 py-5" id="titulo">Practica de Java Script</h1>
+	<button class="btn btn-danger" name="" id="boton" onclick='array()'> "Haz click y veras lo que hace Java Script"</button>
+	<div class="my-5" id="alerta">
+	</div>
+</div>
+<br><br><br><br><br><br>--><!-- practica de array con js  -->
+
+<!-- Practica de Condicional If JS -->
+
+
+ <!--<div class="container text-center py-5" name="Boton1"> 
+	<h1 class="display-3 py-5" id="titulo">Practica de Java Script</h1>
+	<input id="color" type="text" class="form-control my-3" placeholder="ingresa nombre de color">
+	<button class="btn btn-danger" name="" id="boton"> "Haz click y veras lo que hace Java Script"</button>
+	<div class="my-5" id="alerta">
+	</div>
+</div>--><!-- Practica de Condicional If JS -->
+
+<!-- PRACTICA DEL FOR -->
+<!--<div class="container text-center py-5">
+	
+	<h2 id="titulo" class="display-4">FOR</h2>	
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo beatae, odit accusamus magni nostrum obcaecati illum quod, dolorem sint perspiciatis nobis, ea amet necessitatibus, saepe! Aliquid aspernatur nobis saepe doloremque.</p>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo beatae, odit accusamus magni nostrum obcaecati illum quod, dolorem sint perspiciatis nobis, ea amet necessitatibus, saepe! Aliquid aspernatur nobis saepe doloremque.</p>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo beatae, odit accusamus magni nostrum obcaecati illum quod, dolorem sint perspiciatis nobis, ea amet necessitatibus, saepe! Aliquid aspernatur nobis saepe doloremque.</p>
+</div>--><!-- PRACTICA DEL FOR -->
+
+<!-- PRACTICA DEL WHILE -->
+
+<!--<div class="container text-center py-5">
+		<h2 id="titulo">Ciclo While </h2>
+		<div class="py-4">
+			<input type="number" id="numeroCiclos" class="form-control text-center" placeholder="Adivina....?" >	
+		</div>
+			<button id="boton" class="btn btn-success">Enviar</button>
+				<div class="py-4" id="resultado"></div>
+</div>-->
+<!-- PRACTICA DEL WHILE -->
+<!--<div class="form">
+<form name="form1">	
+	<input type="button" name="myButton" value='Boton Pulsado 0 veces' onclick="myButton_onclick()">
+</form>
+</div> -->
+
+<!-- <div class="container text-center py-5">	
+	<h2 id="titulobar" class="display-4"> Animacion de Barra </h2>
+	<div class="py-4">
+		<div class="progress">
+			<div id="barra" class="progress-bar progress-bar-striped inicio animacion" role="progressbar"  aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"> </div>
+		</div>
+	</div>
+	<button id="animacion" class="btn btn-warning"> Animar </button>
+
+</div> --><!-- PRACTICA DEL WHILE -->
+
+<!-- REDES SOCIALES TALLER DE EJEMPLO -->
+
+	<!-- <div class="mt-5 menu-iconos text-center text-white">	
+	<div id="boton" class="bg-warning"><i class="fa fa-share-alt" aria-hidden="true"></i></div>
+	<div id="iconos-sociales" class="">   //esta clase es la que quiero manejar por JS asi que la dejas en BLANCO         
+		<div class="bg-primary"><a href="#"><i class="fa fa-facebook text-white" aria-hidden="true"></i></a></div>
+		<div class="bg-success"><a href="#"><i class="fa fa-twitter text-white" aria-hidden="true"></i></a></div>
+		<div class="bg-danger"><a href="#"><i class="fa fa-youtube text-white" aria-hidden="true"></i></a></div>		
+	</div>
+
+	</div> --> <!-- REDES SOCIALES TALLER DE EJEMPLO -->
+
+
+<!-- Jquery y JS -->
+<script type="text/javascript" src="js/jquery-3.3.1.slim.min.js"></script>
+<script type="text/javascript" src="js/tether.min.js" ></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/scriptpractico.js"></script>
+
+</body>
+</html>
